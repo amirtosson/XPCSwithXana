c35f4e6ea56c15698c31c95dc2712122bfc3da22
Amir Tosson <45110899+amirtosson@users.noreply.github.com>
2021-07-05 09:35:14 +0200

    main class

---
+++ DataManipulation.py
@@ -0,0 +1,37 @@
+"""
+@author:    Amir Tosson
+@license:   GNU General Public License v3 or higher
+@copyright: Universität Siegen, Deutschland
+@email:     tosson@physik.uni-siegen.de
+"""
+
+"""
+summary:    this model contains the functions required 
+            for XPCS data manipulation  
+name:       DataManipulation
+date:       04-07-2021
+"""
+
+import numpy as np
+import h5py
+
+class DataManipulation():
+
+    data_list = []
+
+    def __init__(self):
+        super().__init__
+
+    def GetData(self, filename):
+        with h5py.File(filename, "r") as f:
+            data = f.get('entry/data/data')
+            data_array = np.array(data)
+        return data_array
+
+    def SparseToCOO(self, arr):
+        for i in range(150):
+            arr1 = arr[i, :, :]
+            a = np.transpose(np.nonzero(arr1))
+            b = np.transpose(arr1[[a[:, 0]], [a[:, 1]]])
+            a = np.append(a, b, axis=1)
+            self.data_list.append(a)
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from Xana import Xana
 import numpy as np
+import DataManipulation as _data
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 import pickle
 import h5py
+import concurrent.futures
 
-#f = h5py.File('./data/fw_3_SiO2_PPG_A_00006_data_000001.h5', 'r')
-#print(f.attrs.get('entry'))
 
-with h5py.File('./data/fw_3_SiO2_PPG_A_00006_data_000001.h5', "r") as f:
-    # List all groups
-    itemsS = list(f.items())
-    print("Items: %s" % itemsS)
-    print("Keys: %s" % f.keys())
-    data = f.get('entry')
-    dataarr = np.array(data.get('data/data'))
+def main():
+    data = _data.DataManipulation()
+    data.SparseToCOO(data.GetData('fw_3_SiO2_PPG_A_00006_data_000001.h5'))
+    
+    print(data.data_list)
+    
+    # v = [ data[0:1000,:,:], data[1001:2000,:,:],data[2001:3000,:,:],data[3001:4000,:,:],data[4001:4999,:,:]]
+    # with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+    # executor.map(SparseToCOO, v)
 
-    print('s ', dataarr.shape)
-    #a_group_key = list(f.keys())[0]
+    # a_group_key = list(f.keys())[0]
 
     # Get the data
-    #data = list(f[a_group_key])
-    #print(data)
+    # data = list(f[a_group_key])
+    # print(data)
+
+
 """
 ana = Xana(fmtstr='p10_eiger_h5', detector='eiger500k', setupfile='./setupfiles/setup_SiO2_test.pkl')
 
@@ -34,3 +37,6 @@ plt.subplots()
 plt.imshow(I*ana.setup.mask, norm=LogNorm())
 plt.show()
 """
+
+if __name__ == '__main__':
+    main()
